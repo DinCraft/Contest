abf055194649a492fe4295a19b100b23b2b2b311
DinCraft <idiniyar2006@mail.ru>
2025-02-25 12:08:52 +0300

    working on cleaning list

--- src/task1.c
+++ src/task1.c
@@ -1,25 +1,123 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define METHOD(type, name) _method_##type##_##name
+#define METHOD_NO_ARGS(type, name) _method_##type##_##name(struct type *object)
+#define METHOD(type, name, ...) _method_##type##_##name(struct type *object, __VA_ARGS__)
+#define CONSTRUCTOR(type) _constructor_##type(struct type *object)
 
-struct SElement
+struct SingleLinkedElement
 {
 	int data;
-	struct SElement *next;
+	struct SingleLinkedElement *next;
 };
 
 struct SingleLinkedCyclicList
 {
-	struct SElement *head;
+	struct SingleLinkedElement *head;
 };
 
-void METHOD(SingleLinkedCyclicList, append) ()
+void CONSTRUCTOR(SingleLinkedCyclicList)
 {
+	object->head = NULL;
+}
 
+int METHOD_NO_ARGS(SingleLinkedCyclicList, get_length)
+{
+	int length = 1;
+	if (object->head == NULL)
+	{
+		return 0;
+	}
+	struct SingleLinkedElement *element = object->head;
+	while (element->next != object->head)
+	{
+		element = element->next;
+		length++;
+	}
+	return length;
+}
+
+void METHOD_NO_ARGS(SingleLinkedCyclicList, free)
+{
+	if (object == NULL)
+	{
+		return;
+	}
+
+}
+
+void METHOD(SingleLinkedCyclicList, append, int value)
+{
+	struct SingleLinkedElement *element = object->head;
+	if (element == NULL)
+	{
+		element = malloc(sizeof(struct SingleLinkedElement));
+		element->data = value;
+		element->next = element;
+		object->head = element;
+		return;
+	}
+	while (element->next != object->head)
+	{
+		element = element->next;
+	}
+	struct SingleLinkedElement *newElement = malloc(sizeof(struct SingleLinkedElement));
+	newElement->data = value;
+	newElement->next = object->head;
+	element->next = newElement;
 }
 
 int main()
 {
-	printf("123");
-	_method_SingleLinkedCyclicList_append();
+	struct SingleLinkedCyclicList list;
+	_constructor_SingleLinkedCyclicList(&list);
+	char c;
+	while (1)
+	{
+		scanf("%c", &c);
+		if (c == ' ') {
+			continue;
+		}
+		if (c == '.') {
+			break;
+		}
+		_method_SingleLinkedCyclicList_append(&list, c - '0');
+	}
+	int k;
+	scanf("%d", &k);
+	struct SingleLinkedElement *e = list.head;
+	int length = _method_SingleLinkedCyclicList_get_length(&list);
+	if (length == 0)
+	{
+		printf("0");
+		return 0;
+	}
+	for (int i = 0; i < length - 1; i++)
+	{
+		printf("%d ", e->data);
+		e = e->next;
+	}
+	printf("%d\n", e->data);
+	e = list.head;
+	int is_found = 0;
+	for (int i = 0; i < length; i++)
+	{
+		if (e->data == k)
+		{
+			is_found = 1;
+			break;
+		}
+		e = e->next;
+	}
+	if (!is_found)
+	{
+		e = list.head;
+	}
+	for (int i = 0; i < length - 1; i++)
+	{
+		printf("%d ", e->data);
+		e = e->next;
+	}
+	printf("%d", e->data);
+	return 0;
 }
